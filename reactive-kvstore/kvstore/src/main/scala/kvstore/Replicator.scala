6719e7ee2458f51b758ba97ed332ea454a0d0fdf
Tuan Anh Nguyen <nguyentuananh@anduintransact.com>
2021-07-17 22:02:20 +0700

    Implement replicator actor

--- reactive-kvstore/kvstore/src/main/scala/kvstore/Replicator.scala
+++ reactive-kvstore/kvstore/src/main/scala/kvstore/Replicator.scala
@@ -12,21 +12,29 @@ object Replicator {
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
   case class SnapshotAck(key: String, seq: Long)
 
+  case object Retransmit
+
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
 }
 
 class Replicator(val replica: ActorRef) extends Actor {
-  import Replicator._
   import context.dispatcher
-  
+
+  context.system.scheduler.scheduleAtFixedRate(
+    200.milliseconds,
+    200.milliseconds,
+    self,
+    Replicator.Retransmit
+  )
+
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
 
   // map from sequence number to pair of sender and request
-  var acks = Map.empty[Long, (ActorRef, Replicate)]
+  var acks = Map.empty[Long, (ActorRef, Replicator.Replicate)]
   // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
-  var pending = Vector.empty[Snapshot]
+  var pending = Vector.empty[Replicator.Snapshot]
   
   var _seqCounter = 0L
   def nextSeq() = {
@@ -38,7 +46,28 @@ class Replicator(val replica: ActorRef) extends Actor {
   
   /* TODO Behavior for the Replicator. */
   def receive: Receive = {
-    case _ =>
+    case rep: Replicator.Replicate => handleReplicate(rep)
+    case Replicator.SnapshotAck(key, seq) => handleSnapshotAck(key, seq)
+    case Replicator.Retransmit => handleRetransmit()
+  }
+
+  private def handleReplicate(replicate: Replicator.Replicate): Unit = {
+    val seq = nextSeq()
+    replica ! Replicator.Snapshot(replicate.key, replicate.valueOption, seq)
+    acks += seq -> (context.sender, replicate)
+  }
+
+  private def handleSnapshotAck(key: String, seq: Long): Unit = {
+    acks.get(seq).foreach { case (sender, replicateMsg) =>
+      sender ! Replicator.Replicated(key, replicateMsg.id)
+      acks -= seq
+    }
+  }
+
+  private def handleRetransmit(): Unit = {
+    acks.foreach { case (seq, (_, replicateMsg)) =>
+      replica ! Replicator.Snapshot(replicateMsg.key, replicateMsg.valueOption, seq)
+    }
   }
 
 }
